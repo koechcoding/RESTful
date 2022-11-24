cf9109462183842ef417882fe52a39eaf1f10025
koechcoding <koechkelvin2001@gmail.com>
2022-11-24 20:17:07 +0300

    restful

--- controllers/authors.js
+++ controllers/authors.js
@@ -3,3 +3,7 @@ swagger = require("swagger-node-restify")
 
 function BookSales() {
 }
+BookSales.prototype = new BaseController();
+module.exports = function(lib){
+    var controller = new BookSales();
+}
