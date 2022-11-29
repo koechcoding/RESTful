9e41cfd56ba41bf196cc34b887d5c12bf744f454
koechcoding <koechkelvin2001@gmail.com>
2022-11-29 06:03:25 +0300

    restful booksales

--- controllers/booksales.js
+++ controllers/booksales.js
@@ -14,6 +14,8 @@ module.exports = function(lib){
         'params': [swagger.queryParam('start_date', 'Filter sales done after (or on)this date', 'string'),
                     swagger.queryParam('end_date', 'Filter sales done on or before this date', 'string'),
                     swagger.queryParam('store_id', 'Filter sales done on this store', 'string')
-          ]
+          ],
+        'responseClass': 'BookSale',
+        'nickname': 'getBookSales'
     })
 }
