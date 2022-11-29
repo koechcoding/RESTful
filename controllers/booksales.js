f23bcc9fa71b5fd21260e1b95569d05f2703d40a
koechcoding <koechkelvin2001@gmail.com>
2022-11-29 06:01:31 +0300

    restful booksales

--- controllers/booksales.js
+++ controllers/booksales.js
@@ -1,5 +1,19 @@
 var BaseController = require("./basecontroller"),
 swagger = require("swagger-node-restify")
 function BookSales(){
-    
+
+}
+BookSales.prototype = new BaseController()
+module.exports = function(lib){
+    var controller = new BaseController();
+
+    controller.addAction({
+        'path': '/booksales',
+        'method': 'GET',
+        'summary': 'Returns the list of book sales',
+        'params': [swagger.queryParam('start_date', 'Filter sales done after (or on)this date', 'string'),
+                    swagger.queryParam('end_date', 'Filter sales done on or before this date', 'string'),
+                    swagger.queryParam('store_id', 'Filter sales done on this store', 'string')
+          ]
+    })
 }
