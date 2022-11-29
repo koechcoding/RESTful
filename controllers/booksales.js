14e38ac93ebe4c5d6fbf53fb9cc14773c920d45b
koechcoding <koechkelvin2001@gmail.com>
2022-11-29 06:08:39 +0300

    restful booksales

--- controllers/booksales.js
+++ controllers/booksales.js
@@ -17,5 +17,25 @@ module.exports = function(lib){
           ],
         'responseClass': 'BookSale',
         'nickname': 'getBookSales'
+    }, function(req, res, next){
+        console.log(req)
+
+        var criteria = {}
+        if(req.params.start_date)
+          criteria.date = {$gte: req.params.start_date}
+        if(req.params.end_date)
+          criteria.date = {$lte: req.params.end_date}
+        if(req.params.store_id)
+        criteria.store = req.params.store_id
+          lib.db.model("Booksale")
+          .find(criteria)
+          .populate('books')
+          .populate('client')
+          .populate('employee')
+          .populate('store')
+          .exec(function(err, sales) {
+          if(err) return next(controller.RESTError('InternalServerError', err))
+          controller.writeHAL(res, sales)
+        })
     })
 }
