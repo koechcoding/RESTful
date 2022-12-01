09d63245e934bd54876d67b6f2bdbcad0e8dac84
koechcoding <koechkelvin2001@gmail.com>
2022-12-01 14:11:23 +0300

    overal request schema

---
+++ request_schemas/booksales.js
@@ -0,0 +1,21 @@
+module.exports = {
+    getbooksales: {
+        validate: 'params',
+        schema: {
+            type: 'object',
+            properties: {
+                start_date: {
+                    type: 'string',
+                    format: 'date'
+                },
+                end_date: {
+                    type: 'string',
+                    format:'date'
+                },
+                store_id: {
+                    type: 'string'
+                }
+            }
+        }
+    }
+}
